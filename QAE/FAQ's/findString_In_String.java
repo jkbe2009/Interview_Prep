518707f98cfebab8c9e53e82431df563de8c1d5f
Karthik Jeyakumar <karthikbe2009@gmail.com>
2019-12-12 17:49:11 -0600

    Day 3

---
+++ QAE/FAQ's/findString_In_String.java
@@ -0,0 +1,43 @@
+
+import java.util.*;
+
+class findString_In_String{
+    
+    public static void main(String[] args) throws Exception {
+    String[][] inpArr = {   {"","art"},
+                            {"karthik", ""},
+                            {null, "art"},
+                            {"karthik", null},
+                            {"karthik", "art"},
+                            { "karthik", "fdaf" },
+                            {"karthik", "thi"},
+                            { "karthik", "thic" },
+                        };
+
+    for(String inp[] : inpArr)
+        System.out.println(strstr(inp[0],inp[1]));
+    }
+
+    public static int strstr(String hay, String needle) {
+        // O(nm) | O(1)
+
+        if (needle == null || needle.length() == 0 || hay == null || hay.length() == 0)
+            return -1;
+        int n = needle.length(), m = hay.length();
+
+        for (int i = 0; i < m - n; i++) {
+            int j = 0;
+
+            while (j < n && needle.charAt(j) == hay.charAt(i + j))
+                j++;
+
+            if (j == n)
+                return i;
+        }
+
+        return -1;
+    }
+
+}
+
+
---
+++ QAE/FAQ's/zztemplate.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+class zztemplate{
+    
+    public static void main(String[] args) throws Exception {
+    int[] inpArr = {};
+
+    for(int inp : inpArr)
+        System.out.println((inp));
+    
+    }
+    
+    
+
+}
+
+
