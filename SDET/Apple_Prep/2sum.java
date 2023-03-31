9c64b50ff7b8a0876edd98772e856a62af0b0218
Karthik Jeyakumar <47714674+jkbe2009@users.noreply.github.com>
2023-03-30 20:29:09 -0700

    Day 1

--- SDET/Apple_Prep/2sum.java
+++ SDET/Apple_Prep/2sum.java
@@ -20,7 +20,6 @@ import java.util.*;
 class Solution {
     public static void main(String[] args) {
 
-
         int[] nums = new int[] {2,7,11,15};
         int target = 22;
         int [] res = twoSum(nums, target);
@@ -28,14 +27,12 @@ class Solution {
         for(int i :res)
             System.out.println(i);
 
-
     }
 
 
     public static int[] twoSum(int[] nums, int target){
         //int[] res = new int[2];
         Map<Integer, Integer> map = new HashMap<>();
-
 
         for(int i = 0; i<nums.length; i++){
             int num = target-nums[i];
@@ -65,8 +62,6 @@ class Solution {
 
 
         return new int[] {-1, -1};
-
-
     }
 
 
