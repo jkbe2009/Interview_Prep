3735187d8cd1ad7eebdb2c08404d869b80612e90
Karthik Jeyakumar <47714674+jkbe2009@users.noreply.github.com>
2024-06-07 20:01:03 -0700

    more dp problems

---
+++ SDET/Others_Prep/house_robber_198.py
@@ -0,0 +1,45 @@
+
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """ 
+        dp = [-1] * len(nums)
+        
+        def recursive_soln(house, total):
+            # Base case:
+            if house >= len(nums):
+                return total
+
+            return max(recursive_soln(house+2, total+nums[house]), recursive_soln(house+1, total))
+          
+
+        def iterative_soln():
+            if len(nums) < 2:
+                return max(nums)
+
+            """
+            Using a dp array
+
+            dp = [0] * len(nums)
+            dp[-1] = nums[-1]
+            dp[-2] = max(nums[-2], nums[-1])
+            for i in range(len(nums)-3, -1, -1):
+                dp[i] = max((nums[i]+dp[i+2]), dp[i+1])
+
+            return dp[0]
+            
+            """
+            a = 0
+            b = 0
+
+            for i in range(len(nums)-1, -1, -1):
+                c = max((nums[i]+ a), b)
+                a = b
+                b = c
+
+            return c
+
+        # return recursive_soln(0, 0)
+        return iterative_soln()
