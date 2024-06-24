cbb3a03a13732b1028f3da1efddd3cd20097e4f1
Karthik Jeyakumar <47714674+jkbe2009@users.noreply.github.com>
2024-06-24 11:19:51 -0700

    clean implementation of decode string

--- SDET/Others_Prep/encode_decode_strings_271.py
+++ SDET/Others_Prep/encode_decode_strings_271.py
@@ -4,6 +4,35 @@ class Solution:
     @param: strs: a list of strings
     @return: encodes a list of strings to a single string.
     """
+    
+    def encode(self, strs):
+        res = ""
+
+        for s in strs:
+            res += str(len(s)) + "#" + s
+
+        return res
+
+    def decode(self, s):
+        res = []
+        temp = ""
+        i = 0
+        
+        while i < len(s):
+            if s[i] == '#':
+                l = int(temp)
+                start = i+1
+                end = start+l
+                res.append(s[start:end])
+                i = end
+                temp = ""
+            else:
+                temp += s[i]
+                i += 1
+                
+        return res
+
+    """
     def encode(self, strs):
         # write your code here
         res = ''
@@ -11,10 +40,11 @@ class Solution:
             code = str(len(s))+'#'
             res = res + code + s
         return res
-
+    """
     """
     @param: str: A string
     @return: decodes a single string to a list of strings
+    """
     """
     def decode(self, str):
         # write your code here
@@ -44,3 +74,5 @@ class Solution:
         
         res.append(temp)
         return res
+    """
+
